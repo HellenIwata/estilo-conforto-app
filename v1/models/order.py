d280446e56ad26a530e359ba4dde357ce08c32af
HellenIwata <hellen.iwata001@gmail.com>
2025-11-10 13:14:50 -0300

    Enhance order management with payment details and PDF generation

--- v1/models/order.py
+++ v1/models/order.py
@@ -1,9 +1,39 @@
 # Sistema de gerenciamento de pedidos - Pedidos - V1.0.0
+import datetime
+import os
+from fpdf import FPDF
+from .config_manager_mongo import get_settings
 from .customer import customers, search_customer
 from .catalog import products
 
 orders = {}
 next_id = 1
+
+def get_payment_details(total_value):
+    payment_conditions = input('''
+------ CONDIÇÕES DE PAGAMENTO ------
+[1] À VISTA
+[2] PARCELADO
+''').strip()
+
+    installments = 1
+    installment_value = total_value
+
+    if payment_conditions == '1':
+        payment_conditions = 'À VISTA'
+    elif payment_conditions == '2':
+        payment_conditions = 'PARCELADO'
+        try:
+            installments = int(input("Digite o número de parcelas: "))
+            if installments > 0:
+                installment_value = total_value / installments
+            else:
+                installments = 1
+        except ValueError:
+            print("Número de parcelas inválido. Assumindo 1 parcela.")
+            installments = 1
+
+    payment_method = input("Digite o método de pagamento: ").strip().upper()
 
 
 def insert_order():
@@ -70,20 +100,33 @@ Cancele a operação ou cadastre um cliente primeiro.''')
 
     if new_order_items:
         print("\n--- Detalhes do Pedido ---")
-
         total_value = sum(item['quantity'] * item['unit_price'] for item in new_order_items)
+        print(f"Valor total dos itens: R$ {total_value:.2f}")
+
+        try:
+            discount_str = input("Digite o desconto (R$) ou deixe em branco para 0: ").replace(',', '.')
+            discount = float(discount_str) if discount_str else 0.0
+        except ValueError:
+            print("Valor de desconto inválido. Desconto não aplicado.")
+            discount = 0.0
+
+        final_value = total_value - discount
+        print(f"Valor final com desconto: R$ {final_value:.2f}")
+
+        payment_details = get_payment_details(final_value)
 
         order_id_formatted = f"{next_id:04d}"
         orders[order_id_formatted] = {
             'customer_id': customer_id,
             'items': new_order_items,
-            'total_value': total_value
+            'total_value': final_value,
+            'discount': discount,
+            **payment_details
         }
         print(f"\nPedido ID {order_id_formatted} cadastrado com sucesso para o cliente ID {customer_id}!")
         next_id += 1
     else:
         print("Nenhum item adicionado ao pedido.")
-        
 
 
 def _print_order_details(order_id, info):
@@ -142,13 +185,19 @@ def search_order():
     return search_results_ids
 
 
+def get_order_by_id(order_id):
+    if order_id in orders:
+        return orders[order_id]
+
 def update_order():
     print("\n--- Atualizar Pedido ---")
     if not orders:
         print("Nenhum pedido cadastrado.")
         return
 
-    found_ids = search_order()
+    order_id = input("Digite o ID do pedido que deseja atualizar: ")
+
+    found_ids = get_order_by_id(order_id)
 
     if not found_ids:
         return
@@ -228,6 +277,116 @@ O que você deseja fazer?
 
         else:
             print("Opção inválida.")
+
+def generate_order_pdf_by_id(order_id, order_data):
+    settings = get_settings()
+    logo_path = '../assets/logo2.png'
+    save_dir = settings.get('pdf_output_path', '../output/pdfs')
+    
+    customer = customers.get(order_data['customer_id'], {})
+    customer_name = customer.get('nome', 'N/A')
+    customer_phone = customer.get('telefone', 'N/A')
+    order_discount = order_data.get('discount', 0)
+
+    # --- CREATE PDF ---
+    pdf = FPDF()
+    pdf.add_page()
+
+    # --- HEADER ---
+    if logo_path and os.path.exists(logo_path):
+        pdf.image(logo_path, x=10, y=8, w=25)
+    pdf.set_xy(40, 10)
+    pdf.set_font("Helvetica", "B",size=16)
+    pdf.cell(0, 15, "PEDIDO", ln=True, align="L")
+
+    pdf.set_line_width(0.5)
+    pdf.line(10,35,200,35)
+    pdf.ln(10)
+
+    # --- BODY ---
+    pdf.set_font("Helvetica", "B", 14)
+    pdf.cell(0, 10, f"PEDIDO Nº: {order_id}", ln=True)
+    pdf.set_font("Helvetica", "", 12)
+    pdf.cell(0, 8, f"DATA: {datetime.now().strftime('%d/%m/%Y')}", ln=True)
+    pdf.cell(0, 8, f"CLIENTE: {customer_name}", ln=True)
+    pdf.cell(0, 8, f"TELEFONE: {customer_phone}", ln=True)
+    pdf.ln(5)
+
+    pdf.set_line_width(0.6)
+    pdf.line(10, pdf.get_y(), 200, pdf.get_y())
+    pdf.ln(8)
+
+    # --- TABLE HEADER  ---
+    if not order_data['items']:
+        pdf.set_font("Helvetica", "B", 14)
+        pdf.cell(0,10, "NENHUM ITEM NESTE PEDIDO", align="C", ln=True)
+    else:
+        pdf.set_font("Helvetica", "B", 11)
+        pdf.cell(10, 10, "ID", border=0, align="C")
+        pdf.cell(80, 10, "DESCRIÇÃO PRODUTO", border=0, align="L")
+        pdf.cell(30, 10, "VALOR UNIT.", border=0, align="R")
+        pdf.cell(30, 10, "QTD", border=0, align="C")
+        pdf.cell(30, 10, "VALOR TOTAL", border=0, align="R")
+        pdf.ln(8)
+        pdf.set_line_width(0.4)
+        pdf.line(10, pdf.get_y(), 200, pdf.get_y())
+        pdf.ln(4)
+        
+
+        # --- Order Details ---
+        pdf.set_font("Helvetica", "", 10)
+        subtotal = 0
+        for idx, item in enumerate(order_data['items'], start=1):
+            desc = item['description']
+            unit_price = item['unit_price']
+            quantity = item['quantity']
+            total_item = unit_price * quantity
+            subtotal += total_item
+
+            pdf.cell(10,8, str(idx), border=0, align="C")
+            pdf.multi_cell(80, 8, desc, border=0, align="L")
+            y = pdf.get_y()
+            pdf.set_xy(100, y - 8)
+            pdf.cell(30, 8, f"R$ {unit_price:.2f}", border=0, align="R")
+            pdf.cell(30, 8, str(quantity), border=0, align="C")
+            pdf.cell(30, 8, f"R$ {total_item:.2f}", border=0, align="R")
+            pdf.ln(8)
+        pdf.ln(5)
+        pdf.dashed_line(10, pdf.get_y(), 200, pdf.get_y(), 1, 1)
+        pdf.ln(5)
+
+    # --- Total --
+    payments_conditions = order_data.get('payment_conditions', 'À VISTA')
+    payment_method = order_data.get('payment_method', 'PIX')
+    payment_discount = order_data.get('discount', 0.0)
+    total_final = subtotal - payment_discount
+    
+    pdf.set_font("Helvetica", "B", 11)
+    pdf.cell(70, 8, "CONDIÇÕES DE PAGAMENTO: ", align="L")
+    pdf.set_font("Helvetica", "", 11)
+    pdf.cell(50, 8, payments_conditions, ln=True)
+
+    pdf.set_font("Helvetica", "B", 11)
+    pdf.cell(70,8, "METODO DE PAGAMENTO: ", align="L")
+    pdf.set_font("Helvetica", "", 11)
+    pdf.cell(50, 8, payment_method, ln=True)
+
+    pdf.set_font("Helvetica", "B", 11)
+    pdf.cell(30, 8, "SUBTOTAL: R$ ", border=0, align="R")
+    pdf.cell(40, 8, f"{subtotal:.2f}", align="R", ln=True)
+
+    pdf.cell(140,8, "DESCONTO", align="R")
+    pdf.cell(40, 8, f"R$ {order_discount:.2f}", align="R", ln=True)
+    pdf.dashed_line(10, pdf.get_y(), 200, pdf.get_y(), 1, 1)
+
+    pdf.cell(140, 8, "VALOR TOTAL", align="R")
+    pdf.cell(40, 8, f"R$ {total_final:.2f}", align="R", ln=True)
+
+    # --- SALVE ---
+    os.makedirs(save_dir, exist_ok=True)
+    output_path = os.path.join(save_dir, f"pedido_{customer_name}_{order_id}.pdf")
+    pdf.output(output_path)
+    print(f'PDF gerado com sucesso: {output_path}')
 
 
 def main():
